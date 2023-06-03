031b9019549ed1390d9d4560f4735318b8a616e0
Giorgio Collovà <92723626+Gcollova@users.noreply.github.com>
2023-06-03 20:16:17 +0200

    removing firebase

--- dist/error-handler/error-code.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorCode = void 0;
-class ErrorCode {
-}
-exports.ErrorCode = ErrorCode;
-ErrorCode.ValidationError = 'Data does not match schema';
-ErrorCode.NotFound = 'Not Found';
-ErrorCode.UnknownError = 'UnknownError';
